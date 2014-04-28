2d6e8bbc7e629ba4fe4f0b7bace8392780348f27
Benjamin Audren <benjamin.audren@epfl.ch>
2014-04-28 16:30:53 +0200

    moved to wxPython

--- noteorganiser/widgets.py
+++ noteorganiser/widgets.py
@@ -1,20 +1,11 @@
-try:
-    import fltk
-except ImportError:
-    raise ImportError(
-        "You must install fltk, and its Python bindings to use this software")
-import sys
+import wx
+class ExampleFrame(wx.Frame):
+    def __init__(self, parent):
+        wx.Frame.__init__(self, parent)
+        panel = wx.Panel(self)
+        self.quote = wx.StaticText(panel, label="Your quote: ", pos=(20, 30))
+        self.Show()
 
-
-def theCancelButtonCallback(ptr):
-    sys.exit(0)
-
-if __name__ == "__main__":
-    window = fltk.Fl_Window(100,100,200,90)
-    window.label(sys.argv[0])
-    button = fltk.Fl_Button(9,20,180,50)
-    button.label("Hello World")
-    button.callback(theCancelButtonCallback)
-    window.end()
-    window.show(len(sys.argv), sys.argv)
-    fltk.Fl.run()
+app = wx.App(False)
+ExampleFrame(None)
+app.MainLoop()
