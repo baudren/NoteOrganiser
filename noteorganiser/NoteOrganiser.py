b7efd41a34ba75de941f1933cd8f9592cebc85ce
Benjamin Audren <benjamin.audren@epfl.ch>
2014-05-17 12:34:39 +0100

    ground work for panel choosing

--- noteorganiser/NoteOrganiser.py
+++ noteorganiser/NoteOrganiser.py
@@ -1,13 +1,19 @@
 import sys
-import widgets
+from widgets import *
 import wx
 
 
 class NoteOrganiser(wx.Frame):
 
+    states = [
+        'library',  # The starting one, displaying the notebooks
+        'previewing',
+        'editing']
+
     def __init__(self, parent, system_id,  title):
         wx.Frame.__init__(self, parent=parent, id=system_id,
                           title=title)
+        self.state = 'library'
         self.initUI()
 
     def initUI(self):
@@ -42,8 +48,19 @@ class NoteOrganiser(wx.Frame):
         # Add the menu bar to the application
         self.SetMenuBar(menubar)
 
-    def init_panels(self):
-        pass
+    def init_panels(self, state=None):
+        if not state:
+            state = self.state
+        else:
+            if state not in self.states:
+                raise Exception(
+                    "state unknown")
+            try:
+                self.panels[self.state].TearDown()
+            except:
+                pass
+        if state not in self.panels.iterkeys():
+            exec("self.panels[state] = Panel_%s()")
 
     def OnQuit(self, e):
         self.Close()
