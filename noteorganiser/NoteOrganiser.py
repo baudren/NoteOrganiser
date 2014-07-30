f1b015c84b49c993f2ea8bdda830d7027ef96686
Benjamin Audren <benjamin.audren@epfl.ch>
2014-07-30 09:16:39 +0200

    Configuration expanded, recovers list of notebooks

--- noteorganiser/NoteOrganiser.py
+++ noteorganiser/NoteOrganiser.py
@@ -2,7 +2,11 @@ import sys
 from PySide.QtGui import QApplication, QMainWindow
 from PySide.QtGui import QAction, QTabWidget
 from PySide.QtGui import QFrame
+
+# Local module imports
 from frames import Library, Editing
+from logger import create_logger
+from configuration import initialise
 
 
 class NoteOrganiser(QMainWindow):
@@ -13,17 +17,20 @@ class NoteOrganiser(QMainWindow):
         'editing',
         'preview']
 
-    def __init__(self):
+    def __init__(self, logger):
         QMainWindow.__init__(self)
 
+        self.logger = logger
         self.initUI()
         self.initLogic()
         self.show()
 
     def initUI(self):
+        self.logger.info("Starting UI init of %s" % self.__class__.__name__)
         self.initMenuBar()
         self.initStatusBar()
         self.initWidgets()
+        self.logger.info("Finished UI init of %s" % self.__class__.__name__)
 
         self.setGeometry(600, 1000, 1000, 600)
         self.setWindowTitle('Note Organiser')
@@ -63,8 +70,13 @@ class NoteOrganiser(QMainWindow):
 
 
 def main(args):
+    # Define a logger
+    logger = create_logger('INFO')
+
+    # Recover the folder path and the notebooks
+    root, notebooks = initialise(logger)
     application = QApplication(args)
-    main_window = NoteOrganiser()
+    main_window = NoteOrganiser(logger)
     sys.exit(application.exec_())
 
 
--- noteorganiser/configuration.py
+++ noteorganiser/configuration.py
@@ -1,17 +1,42 @@
-import sys
+"""
+.. module:: configuration
+    :synopsys: Recover the list of existing notebooks
+
+.. moduleauthor:: Benjamin Audren <benjamin.audren@gmail.com>
+"""
 import os
 
-def initialise():
 
-    # Platform independent recovery of the home directory
+def initialise(logger):
+    """
+    Platform independent recovery of the main folder and notebooks
+
+    """
+    # Initialise an empty list of notebooks
+    notebooks = []
+
+    # Platform independent recovery of the home directory. It is always put as
+    # a hidden folder, '.noteorganiser', in the unix tradition.
     home = os.path.expanduser("~")
     main = os.path.join(home, '.noteorganiser')
+
     # Check if the folder ".noteorganiser" exists
     if os.path.isdir(main):
-        print main, ' is there!'
+        logger.info("Main folder existed already")
+        # If yes, check if there are already some notebooks
+        for elem in os.listdir(main):
+            if os.path.isfile(os.path.join(main, elem)):
+                logger.info("Found the file %s as a valid notebook" % elem)
+                notebooks.append(elem)
     else:
+        logger.info("Main folder non-existant: creating it now")
         os.mkdir(main)
-        print main, 'was created!'
+
+    # Return both the path to the folder where it is stored, and the list of
+    # notebooks
+    return main, notebooks
 
 if __name__ == "__main__":
-    initialise()
+    from logger import create_logger
+    logger = create_logger()
+    print(initialise(logger))
