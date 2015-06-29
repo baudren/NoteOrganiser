5ecb3cee141c52b3d01753a5bbe9ee70b6967df4
Benjamin Audren <benj_audren@yahoo.fr>
2015-06-29 13:18:38 +0200

    45-main-folder-choice: fixed test initialisation main window

--- noteorganiser/tests/test_main_window.py
+++ noteorganiser/tests/test_main_window.py
@@ -1,11 +1,22 @@
+from PySide import QtGui
+from PySide import QtCore
 # Frame to test
+import pytest
 from ..NoteOrganiser import NoteOrganiser
 
-def test_initialisation(qtbot):
+
+def test_initialisation(qtbot, mock):
     # Specifying a different folder for storing everything (for testing
     # purposes)
-    # Creating a NoteOrganiser and adding it to the bot
-    note = NoteOrganiser()
-    qtbot.addWidget(note)
+    dialog = QtGui.QFileDialog()
+    dialog.setFileMode(QtGui.QFileDialog.Directory)
+    dialog.setOption(QtGui.QFileDialog.ShowDirsOnly)
+    qtbot.addWidget(dialog)
+    def cancel_dialog():
+        qtbot.click(dialog.cancel)
 
+    QtCore.QTimer.singleShot(200, cancel_dialog)
+    note = NoteOrganiser()
+    # Creating a NoteOrganiser and adding it to the bot
+    qtbot.addWidget(note)
 
