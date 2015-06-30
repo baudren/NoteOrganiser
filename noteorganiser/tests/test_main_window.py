738808522662d4ba84185815b90b4bcc5612fec3
Benjamin Audren <benj_audren@yahoo.fr>
2015-06-30 09:47:59 +0200

    45-main-folder-choice: clean-up

--- noteorganiser/tests/test_main_window.py
+++ noteorganiser/tests/test_main_window.py
@@ -9,17 +9,8 @@ from ..NoteOrganiser import NoteOrganiser
 def test_initialisation(qtbot, mocker):
     # Specifying a different folder for storing everything (for testing
     # purposes)
-    #dialog = QtGui.QFileDialog()
-    #dialog.setFileMode(QtGui.QFileDialog.Directory)
-    #dialog.setOption(QtGui.QFileDialog.ShowDirsOnly)
-    #qtbot.addWidget(dialog)
-    #def cancel_dialog():
-        #qtbot.click(dialog.cancel)
-
     home = os.path.expanduser("~")
-    #QtCore.QTimer.singleShot(200, cancel_dialog)
     main = os.path.join(home, '.noteorganiser')
-    print main
     mocker.patch.object(
         QtGui.QFileDialog, 'getExistingDirectory',
         return_value=main)
