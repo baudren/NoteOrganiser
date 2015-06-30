54ee49ce11ada4b3e26bb8a34043a74c81890bd0
Benjamin Audren <benj_audren@yahoo.fr>
2015-06-30 09:33:47 +0200

    45-main-folder-choice: mock FileDialog for tests

--- noteorganiser/tests/test_main_window.py
+++ noteorganiser/tests/test_main_window.py
@@ -1,3 +1,4 @@
+import os
 from PySide import QtGui
 from PySide import QtCore
 # Frame to test
@@ -5,17 +6,23 @@ import pytest
 from ..NoteOrganiser import NoteOrganiser
 
 
-def test_initialisation(qtbot, mock):
+def test_initialisation(qtbot, mocker):
     # Specifying a different folder for storing everything (for testing
     # purposes)
-    dialog = QtGui.QFileDialog()
-    dialog.setFileMode(QtGui.QFileDialog.Directory)
-    dialog.setOption(QtGui.QFileDialog.ShowDirsOnly)
-    qtbot.addWidget(dialog)
-    def cancel_dialog():
-        qtbot.click(dialog.cancel)
+    #dialog = QtGui.QFileDialog()
+    #dialog.setFileMode(QtGui.QFileDialog.Directory)
+    #dialog.setOption(QtGui.QFileDialog.ShowDirsOnly)
+    #qtbot.addWidget(dialog)
+    #def cancel_dialog():
+        #qtbot.click(dialog.cancel)
 
-    QtCore.QTimer.singleShot(200, cancel_dialog)
+    home = os.path.expanduser("~")
+    #QtCore.QTimer.singleShot(200, cancel_dialog)
+    main = os.path.join(home, '.noteorganiser')
+    print main
+    mocker.patch.object(
+        QtGui.QFileDialog, 'getExistingDirectory',
+        return_value=main)
     note = NoteOrganiser()
     # Creating a NoteOrganiser and adding it to the bot
     qtbot.addWidget(note)
