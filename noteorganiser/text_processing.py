80918965e557533644bc8b8a7b474aacd8a04e11
Benjamin Audren <benj_audren@yahoo.fr>
2015-07-28 15:27:05 +0200

    display tags inline with date

--- noteorganiser/text_processing.py
+++ noteorganiser/text_processing.py
@@ -208,7 +208,9 @@ def post_to_markdown(post):
     tags, post = extract_tags_from_post(post)
     edit_date, post = extract_date_from_post(post)
     text.extend(["<p class='blog-post-meta'>",
-                 "%s" % edit_date, "</p>"])
+                 "%s:" % edit_date,
+                 "%s" % ", ".join(["**%s**" % tag for tag in tags]),
+                 "</p>"])
     corpus = extract_corpus_from_post(post)
     text.extend(corpus)
     text.extend(["</article>", "", ""])
